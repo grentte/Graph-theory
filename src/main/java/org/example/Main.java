49b9c600f129c370bc0aad50bf49dd7fabe04568
grentte <danr.zh2@gmail.com>
2025-04-28 22:49:19 +0300

    функция поиска пирамид в бинарных деревьях, меню с выбором варианта выполнения программы

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,37 @@
 package org.example;
 
-//TIP Программа для поиска пирамид среди поддеревьев бинарного дерева
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
 
         BinaryTree tree = new BinaryTree();
-        tree.buildFromConsole();
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Выберите вариант выполнения программы:\n1. Из консоли\n2. Из файла");
+        int choice = console.nextInt();
+        console.nextLine();
+
+        switch (choice) {
+            case 1:
+                tree.buildFromConsole();
+                break;
+            case 2:
+                System.out.println("Введите название файла: ");
+                String filename = console.nextLine();
+                tree.buildFromFile(filename);
+                break;
+            default:
+                System.out.println("Некорректный выбор");
+                return;
+        }
+
+        System.out.println("\nВывод дерева: ");
         tree.printTree(tree.root);
+
+        System.out.println("\nПоиск всех поддеревьев-пирамид: ");
+        PyramidFinder.findPyramids(tree.root);
+
     }
 }
--- src/main/java/org/example/PyramidFinder.java
+++ src/main/java/org/example/PyramidFinder.java
@@ -1,4 +1,57 @@
 package org.example;
 
+import java.util.*;
+
 public class PyramidFinder {
+    public static void findPyramids(TreeNode root) {
+        findPyramidsHelper(root);
+    }
+
+    private static PyramidInfo findPyramidsHelper(TreeNode node) {
+        if (node == null) {
+            return new PyramidInfo(true, true);
+        }
+
+        PyramidInfo left = findPyramidsHelper(node.left);
+        PyramidInfo right = findPyramidsHelper(node.right);
+
+        boolean isMaxHeap = left.isMaxHeap && right.isMaxHeap;
+        boolean isMinHeap = left.isMinHeap && right.isMinHeap;
+
+        if (node.left != null) {
+            if (node.val < node.left.val) {
+                isMaxHeap = false;
+            }
+            if (node.val > node.left.val) {
+                isMinHeap = false;
+            }
+        }
+
+        if (node.right != null) {
+            if (node.val < node.right.val) {
+                isMaxHeap = false;
+            }
+            if (node.val > node.right.val) {
+                isMinHeap = false;
+            }
+        }
+
+        if (isMaxHeap) {
+            System.out.println("Пирамида с корнем в " + node.val + " (макс-куча)");
+        } else if (isMinHeap) {
+            System.out.println("Пирамида с корнем в " + node.val + " (мин-куча)");
+        }
+
+        return new PyramidInfo(isMaxHeap, isMinHeap);
+    }
+
+    private static class PyramidInfo {
+        boolean isMaxHeap;
+        boolean isMinHeap;
+
+        public PyramidInfo(boolean isMaxHeap, boolean isMinHeap) {
+            this.isMaxHeap = isMaxHeap;
+            this.isMinHeap = isMinHeap;
+        }
+    }
 }
